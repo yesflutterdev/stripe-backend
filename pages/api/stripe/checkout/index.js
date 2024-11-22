bec26bbad0cd3ea51940c737ed634a3fc7b140b4
muhammadnaqeeb <muhammadnaqeebawan@gmail.com>
2024-11-22 15:22:53 +0500

    u checkout

--- pages/api/stripe/checkout/index.js
+++ pages/api/stripe/checkout/index.js
@@ -1,20 +1,26 @@
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 
-export async function createPaymentIntent(req, res) {
-  try {
-    const { amount, currency = 'usd', accountId } = req.body;
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { amount, currency = 'usd', accountId } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100, // Convert to cents
-      currency,
-      payment_method_types: ['card'],
-      transfer_data: {
-        destination: accountId, // Connected account ID
-      },
-    });
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100, // Convert to cents
+        currency,
+        payment_method_types: ['card'],
+        transfer_data: {
+          destination: accountId, // Connected account ID
+        },
+      });
 
-    res.status(200).send({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(500).send({ error: error.message });
+      res.status(200).send({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  } else {
+    // Reject methods other than POST
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
